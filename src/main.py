6976fd2a7849ea288272294596785bdd33c84018
TimRiedel <timriedel00@gmail.com>
2025-05-06 14:01:26 +0200

    feat: add maskbench config file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
+import os
+import yaml
+
 from video_chunker import VideoChunker
 
 
 def main():
-    video_chunker = VideoChunker(chunk_length=120, slide=0)
-    video_path = "ted-talks/0080_song.mp4"
-    chunks = video_chunker.chunk_video(video_path)
+    config = load_config()
+    print(config)
+
+
+def load_config():
+    config_file_name = os.getenv("MASKBENCH_CONFIG_FILE", "maskbench-config.yml")
+    config_file_path = os.path.join("/config", config_file_name)
+    
+    with open(config_file_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    if config is None:
+        raise ValueError("Configuration file is empty or not found.")
+    
+    return config
 
 
 if __name__ == "__main__":
