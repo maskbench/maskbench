3058c212efce519508d447c36024017dcd230a5c
TimRiedel <timriedel00@gmail.com>
2025-07-15 17:49:18 +0200

    refactor: set correct coco keypoint names

--- src/keypoint_pairs.py
+++ src/keypoint_pairs.py
@@ -81,23 +81,26 @@ YOLO_KEYPOINT_PAIRS = [
             (2, 4),
         ]
 
+# Depending on the source you look at, there are a different number of keypoints for COCO.
+# We aggreed to follow the YOLO standard and ordering: https://docs.ultralytics.com/tasks/pose/
+# This does not have a neck keypoint, which is the interpolation of the two shoulders and is mainly used for drawing.
+# The order is different from other COCO sources, see also issue: https://github.com/CMU-Perceptual-Computing-Lab/openpose/issues/1560
 COCO_KEYPOINT_NAMES = {
     0: "Nose",
-    1: "Neck",
-    2: "R. Shoulder",
-    3: "R. Elbow",
-    4: "R. Wrist",
+    1: "L. Eye",
+    2: "R. Eye",
+    3: "L. Ear",
+    4: "R. Ear",
     5: "L. Shoulder",
-    6: "L. Elbow",
-    7: "L. Wrist",
-    8: "R. Hip",
-    9: "R. Knee",
-    10: "R. Ankle",
+    6: "R. Shoulder",
+    7: "L. Elbow",
+    8: "R. Elbow",
+    9: "L. Wrist",
+    10: "R. Wrist",
     11: "L. Hip",
-    12: "L. Knee",
-    13: "L. Ankle",
-    14: "R. Eye",
-    15: "L. Eye",
-    16: "R. Ear",
-    17: "L. Ear",
+    12: "R. Hip",
+    13: "L. Knee",
+    14: "R. Knee",
+    15: "L. Ankle",
+    16: "R. Ankle",
 }
