57c3c701d910b6bf46777b58032c891770bb6377
TimRiedel <timriedel00@gmail.com>
2025-07-31 12:12:30 +0200

    docs: refine pose estimator documentation

--- src/models/pose_estimator.py
+++ src/models/pose_estimator.py
@@ -8,6 +8,7 @@ class PoseEstimator(ABC):
     def __init__(self, name: str, config: dict):
         """
         Initialize the PoseEstimator with a name and configuration.
+        
         Args:
             name (str): The name of the estimator (e.g. "YoloPose", "MediaPipe", "OpenPose", ...).
             config (dict): Configuration dictionary for the pose estimator. This can include arbitrary parameters for the model that are necessary for inference (e.g. "confidence_threshold", "weights_file_name", ...). The config parameter "confidence_threshold" is required. This has no effect for MaskAnyonePoseEstimators, because they do not provide confidence scores. If you do not want to filter, set confidence_threshold to 0.
@@ -24,6 +25,12 @@ class PoseEstimator(ABC):
         """
         Abstract method to estimate the pose of a video using the specific pose estimation model.
         This method should be implemented by subclasses.
+        It receives the full path to the input video file and returns a VideoPoseResult object.
+        The user is responsible for the following three steps after creating the initial VideoPoseResult object:
+        1. Assert that the number of frames in the frame results matches the number of frames in the video (call assert_frame_count_is_correct)
+        2. Filter out low confidence keypoints (call filter_low_confidence_keypoints)
+        3. If the config contains a "save_keypoints_in_coco_format" key, convert the keypoints to the COCO format (call utils.convert_keypoints_to_coco_format, providing a mapping from the model output format to the COCO format)
+
         Args:
             video_path (str): The full path to the input video file.
         Returns:
@@ -36,11 +43,23 @@ class PoseEstimator(ABC):
 
     @abstractmethod
     def get_keypoint_pairs(self) -> list:
+        """
+        Abstract method to get the pairs of keypoints that should be connected when visualizing the pose.
+        This method should be implemented by subclasses.
+        There are pre-defined list of keypoint pairs for various models available in the project.
+        
+        Returns:
+            list: A list of tuples, where each tuple contains two integers representing the indices 
+                 of keypoints that should be connected with a line when visualizing the pose.
+                 For example, [(0,1), (1,2)] means that keypoint 0 should be connected to keypoint 1,
+                 and keypoint 1 should be connected to keypoint 2.
+        """
         pass
 
     def assert_frame_count_is_correct(self, video_pose_result: VideoPoseResult, video_metadata: dict):
         """
         Assert that the number of frames in the frame results matches the number of frames in the video. Should be called at the end of the estimate_pose method.
+
         Args:
             frame_results (list): A list of FramePoseResult objects.
             video_metadata (dict): A dictionary containing the video metadata with the key "frame_count".
