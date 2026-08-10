40e46da2199909d5741c944c0fcfaa7f0d9b29f2
Babajide Owoyele <48095018+babajideowoyele@users.noreply.github.com>
2026-08-10 15:17:49 +0200

    test mode - update visualization pipeline

--- src/evaluation/plots/result_table.py
+++ src/evaluation/plots/result_table.py
@@ -1,23 +1,48 @@
-from typing import Dict
 import numpy as np
 import pandas as pd
 from tabulate import tabulate
 
-from metric_result_class import MetricResult
-from evaluation.utils import aggregate_results_over_all_videos
+from evaluation.result_generator import ResultGenerator
+from checkpointer import Checkpointer
+from metric_result_class import COORDINATE_AXIS
 
+def generate_result_table(checkpointer: Checkpointer) -> pd.DataFrame:
+    convert_metric_names = ["Velocity", "Acceleration", "Jerk"]
+    result_generator = ResultGenerator(checkpointer, test=True)
+    metric_names = result_generator.return_metric_names()
+    if not len(metric_names):
+        return pd.DataFrame() # return empty dataframe
 
-def generate_result_table(metric_results: Dict[str, Dict[str, Dict[str, MetricResult]]]) -> pd.DataFrame:
-    aggregated_results = aggregate_results_over_all_videos(metric_results)
+    aggregated_results = {}
+    for metric_name in metric_names:
+        aggregated_results[metric_name] = {}
+        use_mean = any(substr in metric_name.lower() for substr in ['pck'])
 
-    first_metric = list(metric_results.keys())[0]
+        model_names = result_generator.return_model_names_for_metric(metric_name)
+        for model_name in model_names:
+            aggregated_video_results = []
+            for _, metric_result in result_generator.iter_with_metric_model(metric_name, model_name):
+                if metric_name in convert_metric_names: # kinematic magnitudes requires for these
+                    metric_result = metric_result.aggregate([COORDINATE_AXIS], method='vector_magnitude')
+
+                if use_mean:
+                    aggregated_video_results.append(metric_result.aggregate_all(method='mean'))
+                else:
+                    aggregated_video_results.append(metric_result.aggregate_all(method='median'))
+
+            if use_mean:
+                aggregated_results[metric_name][model_name] = np.round(np.mean(aggregated_video_results), decimals=2)
+            else:
+                aggregated_results[metric_name][model_name] = np.round(np.median(aggregated_video_results), decimals=2)
+
+    first_metric = metric_names[0]
     pose_estimators = list(aggregated_results[first_metric].keys())
 
     data = {
         'Pose Estimator': pose_estimators
     }
 
-    for metric_name in metric_results.keys(): # add column for each metric
+    for metric_name in metric_names: # add column for each metric
         data[metric_name] = [aggregated_results[metric_name][estimator] for estimator in pose_estimators]
 
     df = pd.DataFrame(data)
